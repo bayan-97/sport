7b45c0f6d66201e32a58bf7016afb8709f5f367c
bayan-97 <bayanalalem0@gmail.com>
2020-06-23 19:32:57 +0300

    js2

---
+++ js2/myscript.js
@@ -0,0 +1,30 @@
+'use strict';
+var hi= prompt("are you smille now  yes or no  ");
+var ans;
+if (hi=="yes") {
+   ans= 'stay happy';
+  } else {
+  ans = 'please smile and you will be happy';}
+
+  document.write('<h1>' +ans+ '</h1>');
+  var age= prompt("how old are you");
+  var ag;
+if (age<=18) {
+   ag= 'hi pretty children';
+  } else {
+  ag= 'hi good parents';}
+  document.write('<h1>' +ag+ '</h1>');
+  var fav= prompt("what fav is your favourtsport??");
+  var fava;
+if (fav=="tennis") {
+   fava= 'here can help you';
+   document.write('<h1>' +fava+ '</h1>');
+  } else if(fav=="horseback riding") { fava= 'here can help you';
+                                       
+  document.write('<h1>' +fava+ '</h1>');
+  ;}else if(fav=="swimming") { fava= 'here can help you';                                     
+  document.write('<h1>' +fava+ '</h1>');
+  ;}else{ fava="can you choose another sport "
+  document.write('<h1>' +fava+ '</h1>');
+
+  }
